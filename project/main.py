3ca0644a39d1f9def98339d96e21b354e2983a6e
IDGS-803-20000453 <20000453@alumnos.utleon.edu.mx>
2023-03-16 18:11:58 -0600

    Proyecto modificado en su totalidad en project, como lo tiene el profe

--- project/main.py
+++ project/main.py
@@ -1,14 +1,21 @@
+#Importamos la clase Blueprint del módulo flask
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+#Importamos login_required, current_user de flask_security
+from flask_security import login_required, current_user
+#Importamos el decorador login_required de flask_security
+from flask_security.decorators import roles_required
+#Importamos el objeto de la BD desde __init__.py
 from . import db
 
-main = Blueprint('main', __name__)
+main = Blueprint('main',__name__)
 
+#Definimos la ruta a la página principal
 @main.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
 
+#Definimos la ruta a la página de perfil
 @main.route('/profile')
-#@login_required
+@login_required
 def profile():
-    return 'Profile'
+    return render_template('profile.html', name=current_user.name)
--- project/models.py
+++ project/models.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template
+#Importamos el objeto de la base de  desde __init__.py
+from . import db
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required
-from flask_login import UserMixin, RoleMixin
-from. import User, Role
-# Define models
+#Importamos las clases UserMixin y RoleMixin de flask_security
+from flask_security import UserMixin, RoleMixin
 
-roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+#db = SQLAlchemy()
 
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
+#Definiendo la tabla relacional entre usuarios roles
+users_roles = db.Table('users_roles',
+    db.Column('userId', db.Integer, db.ForeignKey('user.id')),
+    db.Column('roleId', db.Integer, db.ForeignKey('role.id')))
 
 class User(db.Model, UserMixin):
+    """User account model"""
+    
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String,(50) nullable=False)
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean)
+    confirmed_at = db.Column(db.DateTime)
+    roles = db.relationship('Role',
+        secondary=users_roles,
+        backref= db.backref('users', lazy='dynamic'))
+
+class Role(RoleMixin, db.Model):
+    """Role model"""
+
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description =  db.Column(db.String(255))
